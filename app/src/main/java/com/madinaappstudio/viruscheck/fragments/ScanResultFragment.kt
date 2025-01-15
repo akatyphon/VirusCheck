b0d46cab879ee330ab444615231d7e8ba2a29524
MrMonsterX <monsterking122333@gmail.com>
2025-01-15 21:39:15 +0530

    StatsFragment.kt added

--- app/src/main/java/com/madinaappstudio/viruscheck/fragments/ScanResultFragment.kt
+++ app/src/main/java/com/madinaappstudio/viruscheck/fragments/ScanResultFragment.kt
@@ -7,10 +7,18 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
+import com.google.firebase.Firebase
+import com.google.firebase.firestore.firestore
+import com.google.firebase.firestore.getField
 import com.madinaappstudio.viruscheck.R
 import com.madinaappstudio.viruscheck.databinding.FragmentScanResultBinding
 import com.madinaappstudio.viruscheck.models.FileReportResponse
+import com.madinaappstudio.viruscheck.models.StatsModel
 import com.madinaappstudio.viruscheck.models.UrlScanReportResponse
+import com.madinaappstudio.viruscheck.utils.SharedPreference
+import com.madinaappstudio.viruscheck.utils.USER_NODE
+import com.madinaappstudio.viruscheck.utils.setLog
+import com.madinaappstudio.viruscheck.utils.showToast
 import java.time.Instant
 import java.time.ZoneId
 import java.time.format.DateTimeFormatter
@@ -21,6 +29,7 @@ class ScanResultFragment : Fragment() {
 
     private var _binding: FragmentScanResultBinding? = null
     private val binding get() = _binding!!
+    private lateinit var preference: SharedPreference
 
     private val args: ScanResultFragmentArgs by navArgs()
 
@@ -35,18 +44,43 @@ class ScanResultFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         val scanReport = args.scanReport
+        preference = SharedPreference(requireContext())
 
         binding.mtScanResultToolbar.setNavigationOnClickListener {
             findNavController().popBackStack()
         }
 
         if (scanReport.fileReportResponse != null) {
+            val attr = scanReport.fileReportResponse.fileData.fileAttributes
+            val status = if (attr!!.lastAnalysisStats.malicious != 0){
+                2
+            } else if (attr.lastAnalysisStats.suspicious != 0) {
+                3
+            } else {
+                1
+            }
+            val model = getStatsModel(attr.typeExtension, status)
+            saveStats(model)
             binding.mtScanResultToolbar.title = "File Scan Result"
             bindFileData(scanReport.fileReportResponse)
         } else {
+            val attr = scanReport.urlScanReportResponse!!.urlData.urlAttributes
+            val status = if (attr!!.lastAnalysisStats.malicious != 0) {
+                2
+            } else if (attr.lastAnalysisStats.suspicious != 0) {
+                3
+            } else {
+                1
+            }
+            when(status){
+                1 -> saveStats(StatsModel(totalScan = 1, url = 1, clean = 1))
+                2 -> saveStats(StatsModel(totalScan = 1, url = 1, malicious = 1))
+                3 -> saveStats(StatsModel(totalScan = 1, url = 1, suspicious = 1))
+            }
             binding.mtScanResultToolbar.title = "URL Scan Result"
-            bindUrlData(scanReport.urlScanReportResponse!!)
+            bindUrlData(scanReport.urlScanReportResponse)
         }
+
     }
 
 
@@ -148,6 +182,77 @@ class ScanResultFragment : Fragment() {
                 String.format(Locale.getDefault(), "%.2f MB", byte / (1024.0 * 1024.0))
             }
         }
+    }
+
+    private fun saveStats(currentStats: StatsModel){
+        Firebase.firestore.collection(USER_NODE).document(preference.getUserId()!!).get()
+            .addOnSuccessListener {
+                val oldStats = it.getField<StatsModel>("stats")
+                val newStats: StatsModel = if (oldStats != null){
+                    mergeStatsModels(currentStats, oldStats)
+                } else {
+                    currentStats
+                }
+                Firebase.firestore.collection(USER_NODE).document(preference.getUserId()!!).set(
+                    mapOf("stats" to newStats)
+                )
+            }
+            .addOnFailureListener {
+                setLog(it.message)
+            }
+    }
+
+    private fun mergeStatsModels(model1: StatsModel, model2: StatsModel): StatsModel {
+        return StatsModel(
+            totalScan = model1.totalScan + model2.totalScan,
+            clean = model1.clean + model2.clean,
+            malicious = model1.malicious + model2.malicious,
+            suspicious = model1.suspicious + model2.suspicious,
+            url = model1.url + model2.url,
+            apk = model1.apk + model2.apk,
+            exe = model1.exe + model2.exe,
+            document = model1.document + model2.document,
+            archive = model1.archive + model2.archive,
+            other = model1.other + model2.other
+        )
+    }
+
+
+    private fun getStatsModel(fileType: String, status: Int) : StatsModel {
+        val statsModel = StatsModel()
+        statsModel.totalScan = 1
+        when(fileType){
+            "apk" -> {
+                statsModel.apk = 1
+            }
+            "exe" -> {
+                statsModel.exe = 1
+            }
+            "zip", "gz", "7z" -> {
+                statsModel.archive = 1
+            }
+            "pdf", "docx", "txt" -> {
+                statsModel.document = 1
+            }
+            else -> {
+                statsModel.other = 1
+            }
+        }
+
+        when(status){
+            1 -> {
+               statsModel.clean = 1
+            }
+            2 -> {
+                statsModel.malicious = 1
+            }
+            3 -> {
+                statsModel.suspicious = 1
+            }
+        }
+
+        return statsModel
+
     }
 
     override fun onDestroy() {
--- app/src/main/java/com/madinaappstudio/viruscheck/fragments/StatsFragment.kt
+++ app/src/main/java/com/madinaappstudio/viruscheck/fragments/StatsFragment.kt
@@ -1,17 +1,29 @@
 package com.madinaappstudio.viruscheck.fragments
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.firebase.Firebase
+import com.google.firebase.firestore.firestore
+import com.google.firebase.firestore.getField
 import com.madinaappstudio.viruscheck.R
 import com.madinaappstudio.viruscheck.databinding.FragmentStatsBinding
+import com.madinaappstudio.viruscheck.models.StatsModel
+import com.madinaappstudio.viruscheck.utils.ProgressLoading
+import com.madinaappstudio.viruscheck.utils.SharedPreference
+import com.madinaappstudio.viruscheck.utils.USER_NODE
+import com.madinaappstudio.viruscheck.utils.showToast
+import java.text.Format
+import java.util.Locale
 
 class StatsFragment : Fragment() {
 
     private var _binding: FragmentStatsBinding? = null
     private val binding get() = _binding!!
+    private lateinit var progressLoading: ProgressLoading
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -19,5 +31,87 @@ class StatsFragment : Fragment() {
     ): View {
         _binding = FragmentStatsBinding.inflate(layoutInflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        progressLoading = ProgressLoading(requireContext(), 0.9f)
+        loadStatsData()
+    }
+
+    private fun loadStatsData(){
+        progressLoading.show()
+        val userId = SharedPreference(requireContext()).getUserId()!!
+        Firebase.firestore.collection(USER_NODE).document(userId).get()
+            .addOnSuccessListener {
+                val stats = it.getField<StatsModel>("stats")
+                if (stats != null) {
+                    bindViewData(stats)
+                } else {
+                    progressLoading.hide()
+                    showToast(requireContext(), "No Record!")
+                }
+            }
+            .addOnFailureListener {
+                progressLoading.hide()
+                showToast(requireContext(), "Failed to load records.")
+            }
+    }
+
+    private fun bindViewData(statsModel: StatsModel){
+        setTotalScan(statsModel.totalScan)
+        setClean(statsModel.clean)
+        setMalicious(statsModel.malicious)
+        setSuspicious(statsModel.suspicious)
+
+        binding.tvStatsFile.text = "${statsModel.file}"
+        binding.tvStatsUrl.text = "${statsModel.url}"
+        binding.tvStatsApk.text = "${statsModel.apk}"
+        binding.tvStatsArchive.text = "${statsModel.archive}"
+        binding.tvStatsDocument.text = "${statsModel.document}"
+        binding.tvStatsOther.text = "${statsModel.other}"
+
+        progressLoading.hide()
+    }
+
+    private fun setTotalScan(count: Int) {
+        val viewUndetected = binding.viewStatsTotalScan
+        viewUndetected.tvStatsScanCount.setTextColor(
+            Color.parseColor("#1B9AF5")
+        )
+        viewUndetected.tvStatsScanTitle.text = "Total Scan"
+        viewUndetected.tvStatsScanCount.text = "$count"
+    }
+
+    private fun setClean(count: Int) {
+        val viewUndetected = binding.viewStatsClean
+        viewUndetected.tvStatsScanCount.setTextColor(
+            Color.parseColor("#22C55E")
+        )
+        viewUndetected.tvStatsScanTitle.text = "Clean"
+        viewUndetected.tvStatsScanCount.text = "$count"
+    }
+
+    private fun setMalicious(count: Int) {
+        val viewUndetected = binding.viewStatsMalicious
+        viewUndetected.tvStatsScanCount.setTextColor(
+            Color.parseColor("#EF4444")
+        )
+        viewUndetected.tvStatsScanTitle.text = "Malicious"
+        viewUndetected.tvStatsScanCount.text = "$count"
+    }
+
+    private fun setSuspicious(count: Int) {
+        val viewUndetected = binding.viewStatsSuspicious
+        viewUndetected.tvStatsScanCount.setTextColor(
+            Color.parseColor("#EAB308")
+        )
+        viewUndetected.tvStatsScanTitle.text = "Suspicious"
+        viewUndetected.tvStatsScanCount.text = "$count"
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
